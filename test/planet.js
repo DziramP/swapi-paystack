b0b03bd07913a761e4135899aa49fd5d1f6b6351
DziramPapka <dziram.papka@konga.com>
2022-01-25 17:08:35 +0100

    Verify that response time does not exceed 3ms

--- test/planet.js
+++ test/planet.js
@@ -5,10 +5,12 @@ const should = require("chai").should();
 chai.use(require("chai-json-schema"));
 const planetData = require("../schema/planetData");
 const planetSchema = require("../schema/planetSchema");
+const { performance } = require("perf_hooks");
 
 describe("GET /planets/3", function () {
   it("Verifies response headers and status code", async function () {
     const response = await request.get("/planets/3");
+    console.log(response.elapsedTime);
     expect(response.statusCode).to.equal(200);
     expect(response.header["content-type"]).to.include("application/json");
     expect(response.header["vary"]).to.contain("Accept");
@@ -34,11 +36,15 @@ describe("GET /planets/3", function () {
     planetData.should.be.jsonSchema(planetSchema);
   });
 
-//   it("Verifies response time does not exceed 3ms", async function () {
-//     const response = await request.get("/planets/3");
-//     const responseTime = response.time();
-//     expect(responseTime).to.be.at.most(3);
-//   });
+  it("Verifies response time does not exceed 3ms", async function () {
+    const startTime = performance.now();
+    await request.get("/planets/3");
+    if((performance.now() - startTime) > 3){
+        console.log("response time exceeded 3ms")
+    }
+    expect(performance.now() - startTime).to.be.lessThan(3);
+    
+  });
 });
 
 describe("POST /planets/3", function () {
@@ -49,7 +55,6 @@ describe("POST /planets/3", function () {
     };
     const response = await request.post("/planets/3", payload);
     expect(response.statusCode).to.equal(405);
-
   });
   it("Verifies that the response message equals 'Method 'POST' not allowed.'", async function () {
     const payload = {
@@ -59,6 +64,15 @@ describe("POST /planets/3", function () {
     const response = await request.post("/planets/3", payload);
     should.exist(response.body);
     expect(response.body.detail.should.be.equal("Method 'POST' not allowed."));
+  });
+});
 
+describe("GET /planets/3", function () {
+  it("Create a test that mocks out the response and returns a different value for the name object", async function () {
+    const response = await request.get("/planets/3");
+    should.exist(response.body);
+    const newName = "Dziram";
+    response.body.name = newName;
+    expect(response.body.name.should.be.equal(`${newName}`));
   });
 });
