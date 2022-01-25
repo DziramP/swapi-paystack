9553f0c5d8717bb05beaba0db2608ececd16cee1
DziramPapka <dziram.papka@konga.com>
2022-01-25 15:28:49 +0100

    Test case to verify the response data

--- test/planet.js
+++ test/planet.js
@@ -1,8 +1,6 @@
 const request = require("supertest")("https://swapi.dev/api");
-var assert = require('assert');
-var expect = require("chai").expect;
-
-
+const expect = require("chai").expect;
+const should = require("chai").should();
 
 describe('GET /planets/3', function() {
     it('Verifies response headers and status code', async function() {
@@ -12,4 +10,22 @@ describe('GET /planets/3', function() {
         expect(response.header["vary"]).to.contain("Accept");
         expect(response.header["vary"]).to.contain("Cookie");
     })
+
+    it('Verifies the response data', async function() {
+        const response = await request.get("/planets/3");
+        should.exist(response.body);
+        response.body.should.be.a('object');
+        expect(response.body.should.have.property('url'));
+        expect(response.body.should.have.property('climate'));
+        expect(response.body.should.have.property('population'));
+        expect(response.body.should.have.property('diameter'));
+        expect(response.body.should.have.property('orbital_period'));
+        expect(response.body.should.have.property('rotation_period'));
+    })
+
+     // it('Verifies response time', async function() {
+    //     var response = await request.get("/planets/3");
+    //     const responseTime = response.time();
+    //     expect(responseTime).to.be.at.most(3)
+    // })
 })
